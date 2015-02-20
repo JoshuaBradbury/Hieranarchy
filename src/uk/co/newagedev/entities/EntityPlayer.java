2d7375ff73296483adba8b79919e9e4a4f95fc25
joshbradbury52 <joshbradbury7@hotmail.co.uk>
2015-02-20 19:58:54 +0000

    Fix

---
+++ src/uk/co/newagedev/entities/EntityPlayer.java
@@ -0,0 +1,11 @@
+package uk.co.newagedev.entities;
+
+import uk.co.newagedev.hieranarchy.util.Location;
+
+public class EntityPlayer extends Entity {
+
+	public EntityPlayer(Location loc) {
+		super(loc);
+	}
+
+}
