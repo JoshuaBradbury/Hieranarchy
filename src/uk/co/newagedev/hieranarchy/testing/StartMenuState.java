2e6d34c1d0dcb3a17cb284a57e62ab3105ab0c69
joshbradbury52 <joshbradbury7@hotmail.co.uk>
2015-02-21 21:33:52 +0000

    Began work on example start menu.

---
+++ src/uk/co/newagedev/hieranarchy/testing/StartMenuState.java
@@ -0,0 +1,35 @@
+package uk.co.newagedev.hieranarchy.testing;
+
+import uk.co.newagedev.hieranarchy.graphics.Screen;
+import uk.co.newagedev.hieranarchy.state.MenuState;
+import uk.co.newagedev.hieranarchy.ui.Button;
+
+public class StartMenuState extends MenuState {
+
+	private int count, offset;
+	
+	public StartMenuState() {
+		registerComponent(new Button("Test", 100, 100, 200, 50));
+	}
+	
+	@Override
+	public void render() {
+		Screen.renderQuad(0, 0, Main.WIDTH, Main.HEIGHT, new float[] {0.8f, 0.8f, 0.8f});
+		for (int i = -200; i < Main.WIDTH; i += 20) {
+			Screen.renderLine(new int[] {200 + i + offset, 0}, new int[] {i + offset, Main.HEIGHT}, 10.0f, new float[] {0.6f, 0.6f, 0.6f});
+		}
+		super.render();
+	}
+	
+	@Override
+	public void update() {
+		count++;
+		if (count % 5 == 0) {
+			count = 1;
+			offset += 1;
+			if (offset >= 20) {
+				offset = 0;
+			}
+		}
+	}
+}
