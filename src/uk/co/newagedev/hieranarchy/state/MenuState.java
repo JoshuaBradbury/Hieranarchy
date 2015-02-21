49c995062eb09f138de94c462160d85dc92d059f
joshbradbury52 <joshbradbury7@hotmail.co.uk>
2015-02-21 13:38:25 +0000

    Started work on menu states.

---
+++ src/uk/co/newagedev/hieranarchy/state/MenuState.java
@@ -0,0 +1,15 @@
+package uk.co.newagedev.hieranarchy.state;
+
+public class MenuState implements State {
+
+	@Override
+	public void render() {
+		
+	}
+
+	@Override
+	public void update() {
+		
+	}
+
+}
