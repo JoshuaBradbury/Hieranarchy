a5117838eb4faaaffd157e75851eeee20b0aacbf
joshbradbury52 <joshbradbury7@hotmail.co.uk>
2015-02-21 14:04:24 +0000

    Added basic component handling.

--- src/uk/co/newagedev/hieranarchy/state/MenuState.java
+++ src/uk/co/newagedev/hieranarchy/state/MenuState.java
@@ -1,15 +1,33 @@
 package uk.co.newagedev.hieranarchy.state;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import uk.co.newagedev.hieranarchy.ui.Component;
+
 public class MenuState implements State {
 
+	public List<Component> components = new ArrayList<Component>();
+	
 	@Override
 	public void render() {
-		
+		for (Component component : components) {
+			component.render();
+		}
 	}
 
 	@Override
 	public void update() {
-		
+		for (Component component : components) {
+			component.update();
+		}
 	}
 
+	public void registerComponent(Component component) {
+		components.add(component);
+	}
+	
+	public void removeComponent(Component component) {
+		components.remove(component);
+	}
 }
