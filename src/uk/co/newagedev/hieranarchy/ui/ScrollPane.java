2627821dc5499c2be5a68618a0cd6072cad3cb19
joshbradbury52 <joshbradbury7@hotmail.co.uk>
2015-02-23 22:00:12 +0000

    Fixed rendering and updating of the container and scroll bars

--- src/uk/co/newagedev/hieranarchy/ui/ScrollPane.java
+++ src/uk/co/newagedev/hieranarchy/ui/ScrollPane.java
@@ -4,6 +4,7 @@ import uk.co.newagedev.hieranarchy.graphics.Screen;
 
 public class ScrollPane extends Component {
 	private ScrollBar[] scrollBars = new ScrollBar[2];
+	private Container pane;
 	
 	public ScrollPane(int x, int y, int width, int height, int scrollBarDisplays) {
 		super(x, y, width, height);
@@ -13,14 +14,31 @@ public class ScrollPane extends Component {
 		if ((scrollBarDisplays & ScrollBar.VERTICAL) == 1) {
 			scrollBars[1] = new ScrollBar(ScrollBar.VERTICAL, this);
 		}
+		pane = new Container(0, 0);
+	}
+	
+	public Container getPane() {
+		return pane;
+	}
+	
+	public void update() {
+		super.update();
+		for (ScrollBar bar : scrollBars) {
+			if (bar != null) {
+				bar.update();
+			}
+		}
+		pane.update();
 	}
 	
 	public void render() {
+		super.render();
 		Screen.renderQuad((int) getLocation().getX(), (int) getLocation().getY(), (int) getDimensions().getWidth(), (int) getDimensions().getHeight(), Component.LIGHT);
 		for (ScrollBar bar : scrollBars) {
 			if (bar != null) {
 				bar.render();
 			}
 		}
+		pane.render();
 	}
 }
