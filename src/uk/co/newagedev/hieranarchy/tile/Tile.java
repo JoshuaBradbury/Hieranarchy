66ec50d0f97fbdcb2fae3801150b1d1c28ac673c
joshbradbury52 <joshbradbury7@hotmail.co.uk>
2015-03-21 21:33:59 +0000

    Started work on properties for tiles

--- src/uk/co/newagedev/hieranarchy/tile/Tile.java
+++ src/uk/co/newagedev/hieranarchy/tile/Tile.java
@@ -1,5 +1,7 @@
 package uk.co.newagedev.hieranarchy.tile;
 
+import java.util.HashMap;
+
 import uk.co.newagedev.hieranarchy.map.Map;
 import uk.co.newagedev.hieranarchy.util.Location;
 
@@ -7,6 +9,7 @@ public class Tile {
 	private Location loc;
 	private Map map;
 	protected String sprite;
+	private java.util.Map<String, Object> properties = new HashMap<String, Object>();
 	
 	public Tile(Location loc, String sprite) {
 		this.loc = loc;
@@ -15,6 +18,14 @@ public class Tile {
 	
 	public Location getLocation() {
 		return loc;
+	}
+	
+	public void setProperty(String name, Object value) {
+		properties.put(name, value);
+	}
+	
+	public Object getProperty(String name) {
+		return properties.get(name);
 	}
 	
 	public void update() {
