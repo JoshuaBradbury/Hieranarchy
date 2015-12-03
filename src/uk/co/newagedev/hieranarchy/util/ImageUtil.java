d98d1254598ea6dede5eb81a8d28b0fe78790d65
joshbradbury52 <joshbradbury7@hotmail.co.uk>
2015-12-03 19:39:21 +0000

    Basics Image Utility methods.

---
+++ src/uk/co/newagedev/hieranarchy/util/ImageUtil.java
@@ -0,0 +1,49 @@
+package uk.co.newagedev.hieranarchy.util;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
+
+import org.newdawn.slick.opengl.Texture;
+
+import uk.co.newagedev.hieranarchy.graphics.SpriteRegistry;
+
+public class ImageUtil {
+
+	public static Texture getSectionOfTexture(Texture full, int x, int y, int width, int height) {
+		int tx = 0, ty = 0, tw = full.getImageWidth(), th = full.getImageHeight();
+
+		if (x > 0 && x + width < full.getImageWidth()) {
+			tx = x;
+			tw = width;
+		}
+		if (y > 0 && y + height < full.getImageHeight()) {
+			ty = y;
+			th = height;
+		}
+
+		InputStream in = new ByteArrayInputStream(full.getTextureData());
+		BufferedImage img = null;
+
+		try {
+			img = ImageIO.read(in);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		if (img == null)
+			return null;
+
+		String imgName = "image util temp " + full.getTextureData().length;
+		
+		SpriteRegistry.registerImage(imgName, img.getSubimage(tx, ty, tw, th));
+		
+		Texture result = SpriteRegistry.getSprite(imgName).getTexture();
+		
+		return result;
+	}
+
+}
