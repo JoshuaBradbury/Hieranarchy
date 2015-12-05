4318a978c0aeb06f6eeeb2fb80234beceb7dcfdd
joshbradbury52 <joshbradbury7@hotmail.co.uk>
2015-12-05 23:51:13 +0000

    Started MathUtil

---
+++ src/uk/co/newagedev/hieranarchy/util/MathUtil.java
@@ -0,0 +1,9 @@
+package uk.co.newagedev.hieranarchy.util;
+
+public class MathUtil {
+
+	public static float logab(float base, float value) {
+		return (float) (Math.log(value) / Math.log(base));
+	}
+	
+}
