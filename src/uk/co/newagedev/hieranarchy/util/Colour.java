fc8de9a7a12f7ac200e70654d9c6ac6a14a16153
joshbradbury52 <joshbradbury7@hotmail.co.uk>
2015-12-20 00:52:28 +0000

    Added an easy way to render a particular colour instead of having to write float arrays

---
+++ src/uk/co/newagedev/hieranarchy/util/Colour.java
@@ -0,0 +1,17 @@
+package uk.co.newagedev.hieranarchy.util;
+
+public class Colour {
+
+	private Colour() {
+	}
+
+	public static final float[] BLACK     = new float[] { 0.0f, 0.0f, 0.0f, 1.0f };
+	public static final float[] WHITE     = new float[] { 1.0f, 1.0f, 1.0f, 1.0f };
+	public static final float[] RED       = new float[] { 1.0f, 0.0f, 0.0f, 1.0f };
+	public static final float[] GREEN     = new float[] { 0.0f, 1.0f, 0.0f, 1.0f };
+	public static final float[] BLUE      = new float[] { 0.0f, 0.0f, 1.0f, 1.0f };
+	public static final float[] MAGENTA   = new float[] { 1.0f, 0.0f, 1.0f, 1.0f };
+	public static final float[] YELLOW    = new float[] { 1.0f, 1.0f, 0.0f, 1.0f };
+	public static final float[] TURQUOISE = new float[] { 0.0f, 1.0f, 1.0f, 1.0f };
+	public static final float[] ORANGE    = new float[] { 1.0f, 0.5f, 0.0f, 1.0f };
+}
