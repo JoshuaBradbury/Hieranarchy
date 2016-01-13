23db255555d65b3352720e8dc08f2fe62a97a44e
joshbradbury52 <joshbradbury7@hotmail.co.uk>
2016-01-13 20:22:10 +0000

    Fixed the resizing and changed the opengl imports to be not static

--- src/uk/co/newagedev/hieranarchy/graphics/OpenGLScreen.java
+++ src/uk/co/newagedev/hieranarchy/graphics/OpenGLScreen.java
@@ -1,33 +1,5 @@
 package uk.co.newagedev.hieranarchy.graphics;
 
-import static org.lwjgl.opengl.GL11.GL_BLEND;
-import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
-import static org.lwjgl.opengl.GL11.GL_MODELVIEW;
-import static org.lwjgl.opengl.GL11.GL_ONE_MINUS_SRC_ALPHA;
-import static org.lwjgl.opengl.GL11.GL_PROJECTION;
-import static org.lwjgl.opengl.GL11.GL_QUADS;
-import static org.lwjgl.opengl.GL11.GL_SCISSOR_TEST;
-import static org.lwjgl.opengl.GL11.GL_SRC_ALPHA;
-import static org.lwjgl.opengl.GL11.GL_TEXTURE_2D;
-import static org.lwjgl.opengl.GL11.glBegin;
-import static org.lwjgl.opengl.GL11.glBlendFunc;
-import static org.lwjgl.opengl.GL11.glClear;
-import static org.lwjgl.opengl.GL11.glColor3f;
-import static org.lwjgl.opengl.GL11.glColor4f;
-import static org.lwjgl.opengl.GL11.glDisable;
-import static org.lwjgl.opengl.GL11.glEnable;
-import static org.lwjgl.opengl.GL11.glEnd;
-import static org.lwjgl.opengl.GL11.glLoadIdentity;
-import static org.lwjgl.opengl.GL11.glMatrixMode;
-import static org.lwjgl.opengl.GL11.glOrtho;
-import static org.lwjgl.opengl.GL11.glPopMatrix;
-import static org.lwjgl.opengl.GL11.glPushMatrix;
-import static org.lwjgl.opengl.GL11.glRotatef;
-import static org.lwjgl.opengl.GL11.glScissor;
-import static org.lwjgl.opengl.GL11.glTexCoord2f;
-import static org.lwjgl.opengl.GL11.glTranslatef;
-import static org.lwjgl.opengl.GL11.glVertex2f;
-
 import java.awt.Font;
 import java.awt.Rectangle;
 import java.io.FileInputStream;
@@ -198,28 +170,28 @@ public class OpenGLScreen implements Screen {
 
 	public void renderSprite(String spriteName, Vector2f location, float width, float height, float[] texCoords, float[] colour, float[] rotation) {
 		if (SpriteRegistry.doesSpriteExist(spriteName)) {
-			glEnable(GL_TEXTURE_2D);
+			GL11.glEnable(GL11.GL_TEXTURE_2D);
 			SpriteRegistry.getSprite(spriteName).bind();
-			glColor4f(colour[0], colour[1], colour[2], colour[3]);
-			glPushMatrix();
-			glTranslatef(location.getX(), location.getY(), 0.0f);
-			glRotatef(rotation[0], 1.0f, 0.0f, 0.0f);
-			glRotatef(rotation[1], 0.0f, 1.0f, 0.0f);
-			glRotatef(rotation[2], 0.0f, 0.0f, 1.0f);
-			glBegin(GL_QUADS);
+			GL11.glColor4f(colour[0], colour[1], colour[2], colour[3]);
+			GL11.glPushMatrix();
+			GL11.glTranslatef(location.getX(), location.getY(), 0.0f);
+			GL11.glRotatef(rotation[0], 1.0f, 0.0f, 0.0f);
+			GL11.glRotatef(rotation[1], 0.0f, 1.0f, 0.0f);
+			GL11.glRotatef(rotation[2], 0.0f, 0.0f, 1.0f);
+			GL11.glBegin(GL11.GL_QUADS);
 			{
-				glTexCoord2f(texCoords[1], texCoords[3]);
-				glVertex2f(width, height);
-				glTexCoord2f(texCoords[1], texCoords[2]);
-				glVertex2f(width, 0.0f);
-				glTexCoord2f(texCoords[0], texCoords[2]);
-				glVertex2f(0.0f, 0.0f);
-				glTexCoord2f(texCoords[0], texCoords[3]);
-				glVertex2f(0.0f, height);
+				GL11.glTexCoord2f(texCoords[1], texCoords[3]);
+				GL11.glVertex2f(width, height);
+				GL11.glTexCoord2f(texCoords[1], texCoords[2]);
+				GL11.glVertex2f(width, 0.0f);
+				GL11.glTexCoord2f(texCoords[0], texCoords[2]);
+				GL11.glVertex2f(0.0f, 0.0f);
+				GL11.glTexCoord2f(texCoords[0], texCoords[3]);
+				GL11.glVertex2f(0.0f, height);
 			}
-			glEnd();
-			glPopMatrix();
-			glDisable(GL_TEXTURE_2D);
+			GL11.glEnd();
+			GL11.glPopMatrix();
+			GL11.glDisable(GL11.GL_TEXTURE_2D);
 		}
 	}
 
@@ -228,16 +200,17 @@ public class OpenGLScreen implements Screen {
 	}
 
 	public void renderInit() {
-		glMatrixMode(GL_PROJECTION);
-		glLoadIdentity();
-		glOrtho(0, Main.WIDTH, Main.HEIGHT, 0, 1, -1);
-		glMatrixMode(GL_MODELVIEW);
+		GL11.glViewport(0, 0, Main.WIDTH, Main.HEIGHT);
+		GL11.glMatrixMode(GL11.GL_PROJECTION);
+		GL11.glLoadIdentity();
+		GL11.glOrtho(0, Main.WIDTH, Main.HEIGHT, 0, 1, -1);
+		GL11.glMatrixMode(GL11.GL_MODELVIEW);
 
-		glEnable(GL_BLEND);
-		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+		GL11.glEnable(GL11.GL_BLEND);
+		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
 
-		glClear(GL_COLOR_BUFFER_BIT);
-		glLoadIdentity();
+		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
+		GL11.glLoadIdentity();
 	}
 
 	public void postRender() {
@@ -267,36 +240,36 @@ public class OpenGLScreen implements Screen {
 
 	public void renderQuad(Vector2f loc, int width, int height, float[][] colours) {
 		if (colours[0].length == 4) {
-			glBegin(GL_QUADS);
+			GL11.glBegin(GL11.GL_QUADS);
 			{
-				glColor4f(colours[0][0], colours[0][1], colours[0][2], colours[0][3]);
-				glVertex2f(loc.getX() + width, loc.getY() + height);
-				glColor4f(colours[1][0], colours[1][1], colours[1][2], colours[1][3]);
-				glVertex2f(loc.getX() + width, loc.getY());
-				glColor4f(colours[2][0], colours[2][1], colours[2][2], colours[1][3]);
-				glVertex2f(loc.getX(), loc.getY());
-				glColor4f(colours[3][0], colours[3][1], colours[3][2], colours[1][3]);
-				glVertex2f(loc.getX(), loc.getY() + height);
+				GL11.glColor4f(colours[0][0], colours[0][1], colours[0][2], colours[0][3]);
+				GL11.glVertex2f(loc.getX() + width, loc.getY() + height);
+				GL11.glColor4f(colours[1][0], colours[1][1], colours[1][2], colours[1][3]);
+				GL11.glVertex2f(loc.getX() + width, loc.getY());
+				GL11.glColor4f(colours[2][0], colours[2][1], colours[2][2], colours[1][3]);
+				GL11.glVertex2f(loc.getX(), loc.getY());
+				GL11.glColor4f(colours[3][0], colours[3][1], colours[3][2], colours[1][3]);
+				GL11.glVertex2f(loc.getX(), loc.getY() + height);
 			}
-			glEnd();
+			GL11.glEnd();
 		} else {
-			glBegin(GL_QUADS);
+			GL11.glBegin(GL11.GL_QUADS);
 			{
-				glColor3f(colours[0][0], colours[0][1], colours[0][2]);
-				glVertex2f(loc.getX() + width, loc.getY() + height);
-				glColor3f(colours[1][0], colours[1][1], colours[1][2]);
-				glVertex2f(loc.getX() + width, loc.getY());
-				glColor3f(colours[2][0], colours[2][1], colours[2][2]);
-				glVertex2f(loc.getX(), loc.getY());
-				glColor3f(colours[3][0], colours[3][1], colours[3][2]);
-				glVertex2f(loc.getX(), loc.getY() + height);
+				GL11.glColor3f(colours[0][0], colours[0][1], colours[0][2]);
+				GL11.glVertex2f(loc.getX() + width, loc.getY() + height);
+				GL11.glColor3f(colours[1][0], colours[1][1], colours[1][2]);
+				GL11.glVertex2f(loc.getX() + width, loc.getY());
+				GL11.glColor3f(colours[2][0], colours[2][1], colours[2][2]);
+				GL11.glVertex2f(loc.getX(), loc.getY());
+				GL11.glColor3f(colours[3][0], colours[3][1], colours[3][2]);
+				GL11.glVertex2f(loc.getX(), loc.getY() + height);
 			}
-			glEnd();
+			GL11.glEnd();
 		}
 	}
 
 	public void renderLine(Vector2f point1, Vector2f point2, float thickness, float[] colour) {
-		glColor3f(colour[0], colour[1], colour[2]);
+		GL11.glColor3f(colour[0], colour[1], colour[2]);
 		float t = thickness / 2;
 		int x = (int) point1.getX(), x2 = (int) point2.getX(), y = (int) point1.getY(), y2 = (int) point2.getY();
 		float hyp = (float) Math.sqrt(((x2 - x) * (x2 - x)) + ((y2 - y) * (y2 - y)));
@@ -306,14 +279,14 @@ public class OpenGLScreen implements Screen {
 		points[1] = new float[] { (float) (x + (Math.cos(angle - (Math.PI / 2)) * t)), (float) (y + (Math.sin(angle - (Math.PI / 2)) * t)) };
 		points[2] = new float[] { (float) (x + (Math.cos(angle) * hyp) + (Math.cos(angle + (Math.PI / 2)) * t)), (float) (y + (Math.sin(angle) * hyp) + (Math.sin(angle + (Math.PI / 2)) * t)) };
 		points[3] = new float[] { (float) (x + (Math.cos(angle) * hyp) + (Math.cos(angle - (Math.PI / 2)) * t)), (float) (y + (Math.sin(angle) * hyp) + (Math.sin(angle - (Math.PI / 2)) * t)) };
-		glBegin(GL_QUADS);
+		GL11.glBegin(GL11.GL_QUADS);
 		{
-			glVertex2f(points[2][0], points[2][1]);
-			glVertex2f(points[0][0], points[0][1]);
-			glVertex2f(points[1][0], points[1][1]);
-			glVertex2f(points[3][0], points[3][1]);
+			GL11.glVertex2f(points[2][0], points[2][1]);
+			GL11.glVertex2f(points[0][0], points[0][1]);
+			GL11.glVertex2f(points[1][0], points[1][1]);
+			GL11.glVertex2f(points[3][0], points[3][1]);
 		}
-		glEnd();
+		GL11.glEnd();
 	}
 
 	public void close() {
@@ -321,11 +294,11 @@ public class OpenGLScreen implements Screen {
 	}
 
 	public void startScissor(Vector2f loc, int width, int height) {
-		glEnable(GL_SCISSOR_TEST);
-		glScissor((int) loc.getX(), (int) loc.getY(), width, height);
+		GL11.glEnable(GL11.GL_SCISSOR_TEST);
+		GL11.glScissor((int) loc.getX(), (int) loc.getY(), width, height);
 	}
 
 	public void stopScissor() {
-		glDisable(GL_SCISSOR_TEST);
+		GL11.glDisable(GL11.GL_SCISSOR_TEST);
 	}
 }
