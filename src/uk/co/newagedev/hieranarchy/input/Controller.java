bb48128fb0fdc4f1d6c9e82aaba2ac3a4aabe3b4
joshbradbury52 <joshbradbury7@hotmail.co.uk>
2016-01-21 23:02:32 +0000

    Added basic controller support for mouse movement and basic button pressing

---
+++ src/uk/co/newagedev/hieranarchy/input/Controller.java
@@ -0,0 +1,25 @@
+package uk.co.newagedev.hieranarchy.input;
+
+import java.nio.ByteBuffer;
+import java.nio.FloatBuffer;
+
+import org.lwjgl.glfw.GLFW;
+
+public class Controller {
+	
+	public static void update() {
+		for (int i = GLFW.GLFW_JOYSTICK_1; i < GLFW.GLFW_JOYSTICK_LAST; i++) {
+			if (GLFW.glfwGetJoystickName(i) == null) break;
+			FloatBuffer axis = GLFW.glfwGetJoystickAxes(i);
+			ByteBuffer buttons = GLFW.glfwGetJoystickButtons(i);
+			float x = axis.get(0), y = axis.get(1);
+			x *= Math.abs(x) > 0.3 ? 1 : 0;
+			y *= Math.abs(y) > 0.3 ? 1 : 0;
+			Mouse.simulateLocation(Mouse.getCursorX() + (int) (x * 10.0f), Mouse.getCursorY() + (int) (y * 10.0f));
+			
+			if (buttons.get(0) == 1) {
+				Mouse.simulatePress(Mouse.BUTTON_1);
+			}
+		}
+	}
+}
