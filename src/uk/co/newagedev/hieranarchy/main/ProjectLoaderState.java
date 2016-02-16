1fdf4e9c69808f51bd8a02678d3db73aa9e92657
joshbradbury52 <joshbradbury7@hotmail.co.uk>
2016-02-16 00:19:48 +0000

    Added ability to search projects folder and load the projects in a simplified way

--- src/uk/co/newagedev/hieranarchy/main/ProjectLoaderState.java
+++ src/uk/co/newagedev/hieranarchy/main/ProjectLoaderState.java
@@ -1,28 +1,64 @@
 package uk.co.newagedev.hieranarchy.main;
 
 import java.awt.Rectangle;
+import java.util.ArrayList;
 
+import uk.co.newagedev.hieranarchy.project.Project;
 import uk.co.newagedev.hieranarchy.state.MenuState;
 import uk.co.newagedev.hieranarchy.ui.Button;
 import uk.co.newagedev.hieranarchy.ui.ButtonRunnable;
 import uk.co.newagedev.hieranarchy.ui.Container;
+import uk.co.newagedev.hieranarchy.ui.Label;
 import uk.co.newagedev.hieranarchy.ui.ScrollBar;
 import uk.co.newagedev.hieranarchy.ui.ScrollPane;
 import uk.co.newagedev.hieranarchy.util.Colour;
+import uk.co.newagedev.hieranarchy.util.FileUtil;
+import uk.co.newagedev.hieranarchy.util.Logger;
+import uk.co.newagedev.hieranarchy.util.StringUtil;
 
 public class ProjectLoaderState extends MenuState {
 
 	private int count, offset;
+	private Container container;
+	private ScrollPane pane;
 	
 	public ProjectLoaderState() {
-		ScrollPane pane = new ScrollPane(50, 50, Main.WIDTH - 100, Main.HEIGHT - 100, ScrollBar.VERTICAL | ScrollBar.HORIZONTAL);
-		Container container = pane.getPane();
-		container.addComponent(new Button("Project A", 100, 100, 200, 50, false, new ButtonRunnable() {
+		pane = new ScrollPane(50, 50, Main.WIDTH - 100, Main.HEIGHT - 100, ScrollBar.VERTICAL | ScrollBar.HORIZONTAL);
+		container = pane.getPane();
+		String[] projects = getLoadableProjects();
+		for (int i = 0; i < projects.length; i++) {
+			addProjectToLoad(projects[i]);
+		}
+		registerComponent(pane);
+	}
+	
+	public void addProjectToLoad(String projectName) {
+		Project project = new Project(projectName);
+		Container cont = new Container(0, container.getHeight());
+		cont.addComponent(new Label(projectName, 25, 35));
+		cont.addComponent(new Button("Load Project", pane.getWidth() - 225, 20, 200, 50, false, new ButtonRunnable() {
 			public void run() {
-				
+				Logger.info("Loading project " + StringUtil.surroundWith(projectName, "\""));
+				if (Main.project != null) {
+					Main.project.cleanup();
+				}
+				Main.project = project;
 			}
 		}));
-		registerComponent(pane);
+		cont.update();
+		container.addComponent(cont);
+	}
+	
+	public String[] getLoadableProjects() {
+		String[] projectFolders = FileUtil.getAllFilesInFolder(Project.DIRECTORY);
+		ArrayList<String> loadableProjects = new ArrayList<String>();
+		for (int i = 0; i < projectFolders.length; i++) {
+			String[] projectFolderFileList = FileUtil.getAllFilesInFolder(Project.DIRECTORY + projectFolders[i]);
+			if (StringUtil.doesArrayContainString(projectFolderFileList, Project.PROJECT_FILE)) {
+				loadableProjects.add(projectFolders[i]);
+			}
+		}
+		return loadableProjects.toArray(new String[] {});
 	}
 	
 	@Override
